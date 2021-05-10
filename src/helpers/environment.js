97a768f14ee95fe8109b11c8461e69e62e530b1e
aperkins53 <61627909+aperkins53@users.noreply.github.com>
2021-05-10 18:22:06 -0400

    fixing issues

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -7,9 +7,9 @@ switch (window.location.hostname) {
  APIURL = 'http://localhost:3000';
  break;
  // this is the deployed react application
- case 'sap-client-ap.herokuapp.com':
+ case 'sap-server.herokuapp.com':
  // this is the full url of your deployed API
- APIURL = 'https://sap-client-ap.herokuapp.com'
+ APIURL = 'https://sap-server.herokuapp.com'
 }
 
 export default APIURL;
